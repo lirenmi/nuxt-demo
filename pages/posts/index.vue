543a0fd681381744b7e23a042e7e5dd1451b6033
lirenmi <lirenmi@163.com>
2019-03-20 17:15:10 +0800

    初始化数据方法 asyncData

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -1,12 +1,21 @@
 <template>
   <div class="section">
-    <h1 class="title">List</h1>
+    <h1 class="title is-3">List</h1>
+    <div
+      v-for="post in posts"
+      :key="post.id"
+    >
+      {{ post.title }}
+    </div>
   </div>
 </template>
 
 <script>
   export default {
-
+    async asyncData( { $axios } ) {
+      const posts = await $axios.$get('http://localhost:3333/posts')
+      return { posts }
+    }
   }
 </script>
 
