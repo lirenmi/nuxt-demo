eac80bfd0babb3f954d9df7b89bf644a71018ec2
lirenmi <lirenmi@163.com>
2019-03-20 12:23:14 +0800

    init

---
+++ pages/posts/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="section">
+    <h1 class="title">List</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style scoped>
+</style>
---
+++ pages/posts/post.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="section">
+    <h1 class="title">post</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style scoped>
+</style>
