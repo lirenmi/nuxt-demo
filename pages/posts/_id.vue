b8cdaba9464d8d53eb1eeee1c0fb7f10168bf698
lirenmi <lirenmi@163.com>
2019-03-20 16:00:05 +0800

    动态路由

---
+++ pages/posts/_id.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="section">
+    <h1 class="title is-1">Post id: {{$route.params.id}} </h1>
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style scoped>
+</style>
