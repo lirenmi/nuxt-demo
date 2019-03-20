ee19e860bb061233493eda717a03ac4102f0c6a2
lirenmi <lirenmi@163.com>
2019-03-20 17:17:12 +0800

    视图：自定义布局

--- layouts/blank.vue
+++ layouts/blank.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="section">
-    <h1 class="title">post</h1>
+  <div>
+    <nuxt />
   </div>
 </template>
 
 <script>
   export default {
-    layout: 'blank'
+
   }
 </script>
 
