32875668405cfc036bcd215c6163622bd850cdf2
lirenmi <lirenmi@163.com>
2019-03-21 11:41:14 +0800

    Vuex：使用 State

---
+++ pages/demo/vuex.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="section">
+    <div class="container">
+      <h1 class="title i">{{ pageName }}</h1>
+      <span class="tag is-primary">{{ count }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    computed: {
+      pageName() {
+        return this.$store.state.demo.pageName
+      },
+      count() {
+        return this.$store.state.demo.count
+      }
+    }
+  }
+</script>
+
+<style scoped>
+.title {
+  font-size: 5rem;
+  font-weight: 200;
+}
+</style>
