0ada20327f5c18f3fd21026b3c6d20de86e104f0
lirenmi <lirenmi@163.com>
2019-03-21 12:58:26 +0800

    删除一个Post内容

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -3,29 +3,37 @@
     <div class="container">
       <h1 class="title is-3">List</h1>
       <div class="columns is-multiline is-desktop">
-        <nuxt-link
+        <div
           :to="{name: 'posts-id', params: {id: post.id} }"
           class="column is-3"
           v-for="post in posts"
           :key="post.id"
         >
           <div class="card">
-            <div class="card-image">
-              <figure class="image is-4by3">
-                <img
-                  :src="post.imageUrl"
-                  :alt="post.title"
-                >
-              </figure>
-            </div>
-            <div class="card-content">
-              <p class="title is-5">{{ post.title }}</p>
-              <div class="subtitle is-7">
-                {{ post.author }}
+            <nuxt-link :to="{name: 'posts-id', params: {id: post.id} }">
+              <div class="card-image">
+                <figure class="image is-4by3">
+                  <img
+                    :src="post.imageUrl"
+                    :alt="post.title"
+                  >
+                </figure>
               </div>
-            </div>
+              <div class="card-content">
+                <p class="title is-5">{{ post.title }}</p>
+                <div class="subtitle is-7">
+                  {{ post.author }}
+                </div>
+              </div>
+            </nuxt-link>
+            <footer class="card-footer">
+              <a
+                class="card-footer-item"
+                @click.self="destroyAction(post.id)"
+              >Delete</a>
+            </footer>
           </div>
-        </nuxt-link>
+        </div>
       </div>
     </div>
 
@@ -45,6 +53,11 @@
     computed: {
       posts() {
         return this.$store.state.posts.list
+      }
+    },
+    methods: {
+      destroyAction(id) {
+        this.$store.dispatch('posts/destroyAction', id)
       }
     },
     head() {
--- store/posts.js
+++ store/posts.js
@@ -5,5 +5,15 @@ export const state = () => ({
 export const mutations = {
   setList(state, payload) {
     state.list = payload
+  },
+  removeItem(state, id) {
+    state.list = state.list.filter(item => item.id != id)
+  }
+}
+
+export const actions = {
+  async destroyAction(context, id) {
+    await this.$axios.delete(`http://localhost:3333/posts/${id}`)
+    context.commit('removeItem', id)
   }
 }
